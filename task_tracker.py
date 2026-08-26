a7eb8610e3ba7cbe9ebc2445433bdda37d4ece3c
surajS0426 <suraj26304@gmail.com>
2026-08-26 18:05:10 +0100

    Refactor TaskManager
    Changed Task Manager class to instead instantiate with a dictionary and not a list. Changed methods along with it to utilise the dictionary

--- task_tracker.py
+++ task_tracker.py
@@ -27,26 +27,23 @@ class Task:
 class TaskManager:
     
     def __init__(self):
-        self.tasks = []
+        self.tasks = {}
         
     def add_task(self, task):
-        self.tasks.append(task)
+        self.tasks[task.task_id] = task
         
     def remove_task(self, task_id):
-        self.tasks = [task for task in self.tasks if task.task_id != task_id]
+        self.tasks.pop(task_id, None)
         
     def update_task(self, task_id, **kwargs):
-        for task in self.tasks:
-            if task.task_id == task_id:
-                for key, value in kwargs.items():
-                    if hasattr(task, key):
-                        setattr(task, key, value)
+        task = self.tasks.get(task_id)
+        if task:
+            for key, value in kwargs.items():
+                if hasattr(task, key):
+                    setattr(task, key, value)
                         
     def get_task(self, task_id):
-        for task in self.tasks:
-            if task.task_id == task_id:
-                return task
-        return None
+        return self.tasks.get(task_id)
     
     def update_task_status(self, task_id, new_status):
         task = self.get_task(task_id)
